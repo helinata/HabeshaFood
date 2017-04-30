cfbfffaa7093002303c1a417c58e9076cd990e35
Helinata <linaatsebha@gmail.com>
2017-04-30 11:38:53 -0400

    Changesinclude first deployment to App store

---
+++ HabeshaFood/www/js/app.js
@@ -0,0 +1,90 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html'
+      }
+    }
+  })
+  .state('app.vegetarian', {
+    url: '/vegetarian',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/vegetarian.html'
+      }
+    }
+  })
+
+  .state('app.vegan', {
+      url: '/vegan',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/vegan.html',
+          controller: 'VeganCtrl'
+        }
+      }
+    })
+    .state('app.description', {
+      url: '/description',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/description.html'          
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/recipeList/',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/recipeList.html',
+        controller: 'RecipesCtrl'
+      }
+    }
+})
+  .state('app.recipe', {
+    url: '/recipe/',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/recipe.html',
+        controller: 'RecipeCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
